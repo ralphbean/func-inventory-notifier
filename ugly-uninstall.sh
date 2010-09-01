8a832fb6ed3e61c9254ce83c6f087d76c7e12101
Ralph Bean <ralph.bean@gmail.com>
2010-09-01 17:39:45 -0400

    remove broken pieces

--- ugly-uninstall.sh
+++ ugly-uninstall.sh
@@ -3,3 +3,5 @@ rm -rfv /usr/lib/python*/site-packages/func.overlord.inventory.notifier*
 rm -rfv /etc/func/func-inventory-notifier.conf
 rm -rfv /usr/bin/func-inventory-notifier
 rm -rfv func*egg-info build dist
+rm -rfv /usr/lib/python*/site-packages/func/overlord/inventory
+rm -rfv /usr/lib/python*/site-packages/func/overlord/inventory.pyc
