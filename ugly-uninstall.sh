da12311b7611ce4937954a1964bdebb2fe19b20b
Ralph Bean <ralph.bean@gmail.com>
2010-09-01 01:55:26 -0400

    make uninstall do cleaning too.

--- ugly-uninstall.sh
+++ ugly-uninstall.sh
@@ -2,3 +2,4 @@
 rm -rfv /usr/lib/python*/site-packages/func.overlord.inventory.notifier*
 rm -rfv /etc/func/func-inventory-notifier.conf
 rm -rfv /usr/bin/func-inventory-notifier
+rm -rfv func*egg-info build dist
